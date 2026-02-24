7fd90ce4f042a5ae3874b3ed708200603ff28c13
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-02-23 20:46:53 -0500

    Harden Django settings for Railway host/domain changes

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate --noinput && python manage.py collectstatic --noinput
-web: gunicorn descubriendo.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3 --log-file -
+web: gunicorn descubriendo.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --threads 4 --timeout 120
--- descubriendo/urls.py
+++ descubriendo/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
+
+
+def healthz(_request):
+    return HttpResponse('ok', content_type='text/plain')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('healthz', healthz),
     path('', include('core.urls')),
 ]
+
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
